6efefbb12ea4bdbed4b1161ca795dfe517e4dcb2
shadish <shadtisch@gmail.com>
2019-01-12 12:58:40 -0700

    add Visual Studio Code settings, back up just in case

---
+++ vs-code-settings.js
@@ -0,0 +1,25 @@
+{
+	    "files.autoSave": "onFocusChange",
+		    "editor.fontSize": 16,
+		    "editor.fontFamily": "Anonymous Pro, monospace",
+		    "editor.tabSize": 2,
+		    "editor.insertSpaces": false,
+		    "editor.renderWhitespace": "all",
+		    "editor.cursorStyle": "line",
+		    "editor.wordWrap": "on",
+		    "editor.wordWrapColumn": 80,
+		    "editor.formatOnSave": true,
+		    "editor.scrollBeyondLastColumn": 12,
+		    "workbench.startupEditor": "newUntitledFile",
+		    "breadcrumbs.enabled": true,
+		    "typescript.updateImportsOnFileMove.enabled": "always",
+		    "workbench.colorTheme": "Abyss",
+		    "workbench.iconTheme": "vs-minimal",
+		    "files.eol": "\n",
+		    "extensions.ignoreRecommendations": false,
+		    "vim.handleKeys": {
+			            "<C-a>": false,
+			            "<C-d>": false,
+			            "<C-f>": false
+			        }
+}

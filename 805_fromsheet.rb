dcec439f297ba0b82af486e2fd92e5103bafa40c
shadish <shadtisch@gmail.com>
2023-11-05 18:44:45 -0800

    Update 805 stuff and fps auto hotkeys things.

--- 805_fromsheet.rb
+++ 805_fromsheet.rb
@@ -9,15 +9,30 @@ s2 = 60000
 
 # Cash to close
 calculated = c + s1 + s2
-puts calculated
-puts closing_costs + cash_to_close
 
 discrepency = calculated - (closing_costs + cash_to_close)
-puts discrepency.round 2
-exit
 
-tot_third = calculated * 0.333
+tot_third = calculated/3
 
-c_owe = (tot_third - s1 - s2).round 2
+c_owe = (tot_third - s1 - s2).round(2)* -1
 
-puts "Owed: $#{c_owe}"
+puts "For closing conleys owe shad: #{c_owe}"
+puts "--------"
+
+shad_gpay = 16589.06
+shad_check = 6500
+shad_total = shad_check + shad_gpay
+
+alaska_total_deposit = 42055.91
+best_buy_refund = 1562.64
+alaska_total = (alaska_total_deposit - best_buy_refund).round(2)
+con_total = (alaska_total_deposit - shad_total).round(2)
+
+ak_third = (alaska_total/3).round(2)
+con_acct_owed = (shad_total - ak_third).round(2)
+
+puts "Shad contributed #{shad_total} of #{alaska_total}, so conleys contributed #{con_total}, owing shad #{con_acct_owed}"
+puts "--------"
+tot_shad = con_acct_owed + c_owe
+
+puts "Total payment to shad: #{tot_shad}"
